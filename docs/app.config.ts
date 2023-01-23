6feefb1f2d83716fb65edd0ba2fcd75ba9ce39ac
matteo.tritto <matteo.tritto@weroad.it>
2023-01-23 14:25:52 +0100

    add docs

---
+++ docs/app.config.ts
@@ -0,0 +1,19 @@
+export default defineAppConfig({
+  docus: {
+    title: 'nuxt-battery',
+    description: 'Nuxt module for get status battery in your app.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      github: 'teonji/nuxt-battery'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: []
+    }
+  }
+})
