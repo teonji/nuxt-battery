955c41e72ff9e5739b5895a2ae99825224776e60
matteo.tritto <matteo.tritto@weroad.it>
2023-01-27 20:21:50 +0100

    fix: export types in module

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,11 @@
 import { fileURLToPath } from 'url'
 import { defineNuxtModule, addPlugin, addImportsDir, createResolver } from '@nuxt/kit'
-
+export interface useBattery {
+  status: number
+  charging: boolean | null
+  time: number
+  label: string
+}
 export interface ModuleOptions {
   addPlugin: boolean
 }
